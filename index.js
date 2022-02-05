ddbbbd55811c7ebfe4d0736a4017efb274b382cc
Bridget Kelly <bckelly197@gmail.com>
2022-02-05 14:48:04 -0500

    commit tree 21b501066ffe0087f9df88735b9ae32b00b2b405

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// function newBatteries () {
+// newBatteries = (previous, current) => previous + current;
+// return batteryBatches.reduce(newBatteries);
+// }
+
+// console.log(batteryBatches.reduce(newBatteries));
+
+// function newBatteries () {
+//     let sum = batteryBatches.reduce(function (previous, current) {
+//         console.log(previous)
+//     let add = current + previous;
+//     }, 0)
+// }
+
+const totalBatteries = batteryBatches.reduce(function(previous, current) {
+    return previous + current
+}
+)
